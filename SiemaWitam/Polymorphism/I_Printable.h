a088a44375a7e9610d1ca6f46e6fc0a5000d3438
Siema Maciek <116840498+OwczarekMaciej@users.noreply.github.com>
2023-10-24 23:24:24 +0200

    Polymorphism

---
+++ SiemaWitam/Polymorphism/I_Printable.h
@@ -0,0 +1,18 @@
+#ifndef I_PRINTABLE_H
+#define I_PRINTABLE_H
+#include <iostream>
+
+#pragma once
+
+class I_Printable
+{
+    friend std::ostream &operator<<(std::ostream &os, const I_Printable &obj);
+public:
+    virtual void print(std::ostream &os) const = 0;
+    virtual ~I_Printable() = default;
+
+private:
+
+};
+
+#endif
---
+++ SiemaWitam/Polymorphism/main.cpp
@@ -0,0 +1,31 @@
+// Section 16 
+// Challenge 
+#include <iostream>
+#include <vector>
+#include "Account.h"
+#include "Savings_Account.h"
+#include "Checking_Account.h"
+#include "Trust_Account.h"
+#include "Account_Util.h"
+
+using namespace std; 
+
+int main() {
+    cout.precision(2);
+    cout << fixed;
+   
+    Savings_Account s{"Frank", 5000, 2.6};
+    cout << s << endl;
+    s.deposit(10000);
+    cout << s << endl;
+    s.withdraw(10000);
+    cout << s << endl;
+
+    Account *ptr = new Trust_Account("Leo", 5000, 2.7);
+    cout << *ptr << endl;
+    
+
+    
+    return 0;
+}
+
